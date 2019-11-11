026c31dcbac75960f3b7e6c15af3708dbf85e07e
caiogenzerico <47903982+caiogenzerico@users.noreply.github.com>
2019-11-11 09:46:49 -0300

    initial commit

---
+++ delivery/src/main/java/com/capgemini/batatatinhadelivery/BatatatinhaApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.batatatinhadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatatatinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatatatinhaApplication.class, args);
+	}
+
+}
---
+++ delivery/src/test/java/com/capgemini/batatatinhadelivery/BatatatinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.batatatinhadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatatatinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
